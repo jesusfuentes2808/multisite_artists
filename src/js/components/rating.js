0f902a9b36361868b22109dd348cd714de66d932
joeldasc <joeldiego2895@gmail.com>
2022-01-23 14:25:49 -0500

    add page tanks + fix in file js

--- src/js/components/rating.js
+++ src/js/components/rating.js
@@ -1,44 +1,48 @@
+
 function rateSystem( className, obj, fnc=function(){} ) {
     /* window.myStarCollection.push(className); */
     for( let i=0; i<obj.length; i++ ) {
-        document.getElementsByClassName(className)[i].style.width = (obj[i].rating*obj[i].starSize) + "px";
-        document.getElementsByClassName(className)[i].style.height = obj[i].starSize + "px";
-        document.getElementsByClassName(className)[i].style.backgroundSize=obj[i].starSize + "px";
-        document.getElementsByClassName(className)[i].style.backgroundImage = "url('" + obj[i].starImage + "')" ;
-        document.getElementsByClassName(className)[i].style.backgroundRepeat="repeat-x";
-        document.getElementsByClassName(className)[i].parentElement.style.width = (parseInt(obj[i].starSize)*parseInt(obj[i].maxRating) ) + "px";
-        document.getElementsByClassName(className)[i].parentElement.style.maxWidth = (parseInt(obj[i].starSize)*parseInt(obj[i].maxRating) ) + "px";
-        document.getElementsByClassName(className)[i].parentElement.style.height = parseInt(obj[i].starSize)+"px";
+        const element = document.getElementsByClassName(className)[i];
+        if (typeof(element) != 'undefined' && element != null) {
+            document.getElementsByClassName(className)[i].style.width = (obj[i].rating*obj[i].starSize) + "px";
+            document.getElementsByClassName(className)[i].style.height = obj[i].starSize + "px";
+            document.getElementsByClassName(className)[i].style.backgroundSize=obj[i].starSize + "px";
+            document.getElementsByClassName(className)[i].style.backgroundImage = "url('" + obj[i].starImage + "')" ;
+            document.getElementsByClassName(className)[i].style.backgroundRepeat="repeat-x";
+            document.getElementsByClassName(className)[i].parentElement.style.width = (parseInt(obj[i].starSize)*parseInt(obj[i].maxRating) ) + "px";
+            document.getElementsByClassName(className)[i].parentElement.style.maxWidth = (parseInt(obj[i].starSize)*parseInt(obj[i].maxRating) ) + "px";
+            document.getElementsByClassName(className)[i].parentElement.style.height = parseInt(obj[i].starSize)+"px";
 
-        if ( obj[i].minRating ) {
-            document.getElementsByClassName(className)[i].style.minWidth = (obj[i].minRating*obj[i].starSize) + "px";
-        } else {
-            document.getElementsByClassName(className)[i].style.minWidth = "0px";
+            if ( obj[i].minRating ) {
+                document.getElementsByClassName(className)[i].style.minWidth = (obj[i].minRating*obj[i].starSize) + "px";
+            } else {
+                document.getElementsByClassName(className)[i].style.minWidth = "0px";
+            }
+
+            if ( obj[i].backgroundStarImage ) {
+                document.getElementsByClassName(className)[i].parentElement.style.backgroundSize=obj[i].starSize + "px";
+                document.getElementsByClassName(className)[i].parentElement.style.backgroundRepeat="repeat-x";
+                document.getElementsByClassName(className)[i].parentElement.style.backgroundImage = "url('" + obj[i].backgroundStarImage + "')" ;
+            }
+
+            if ( obj[i].emptyStarImage ) {
+                document.getElementsByClassName(className)[i].innerHTML = '<div class="emptyStarRating"></div>';
+                document.getElementsByClassName(className)[i].getElementsByClassName("emptyStarRating")[0].style.backgroundSize = parseInt(obj[i].starSize) + "px";
+                document.getElementsByClassName(className)[i].getElementsByClassName("emptyStarRating")[0].style.backgroundImage = "url('" + obj[i].emptyStarImage + "')" ;
+                document.getElementsByClassName(className)[i].getElementsByClassName("emptyStarRating")[0].style.backgroundRepeat="repeat-x";
+                document.getElementsByClassName(className)[i].getElementsByClassName("emptyStarRating")[0].style.width = (parseInt(obj[i].starSize)*parseInt(obj[i].maxRating) ) + "px";
+                document.getElementsByClassName(className)[i].getElementsByClassName("emptyStarRating")[0].style.height = parseInt(obj[i].starSize)+"px";
+            }
+
+            document.getElementsByClassName(className)[i].style.maxWidth = obj[i].starSize*obj[i].maxRating + "px";
+            document.getElementsByClassName(className)[i].dataset.rating = obj[i].rating;
+            document.getElementsByClassName(className)[i].dataset.step = obj[i].step;
         }
-
-        if ( obj[i].backgroundStarImage ) {
-            document.getElementsByClassName(className)[i].parentElement.style.backgroundSize=obj[i].starSize + "px";
-            document.getElementsByClassName(className)[i].parentElement.style.backgroundRepeat="repeat-x";
-            document.getElementsByClassName(className)[i].parentElement.style.backgroundImage = "url('" + obj[i].backgroundStarImage + "')" ;
-        }
-
-        if ( obj[i].emptyStarImage ) {
-            document.getElementsByClassName(className)[i].innerHTML = '<div class="emptyStarRating"></div>';
-            document.getElementsByClassName(className)[i].getElementsByClassName("emptyStarRating")[0].style.backgroundSize = parseInt(obj[i].starSize) + "px";
-            document.getElementsByClassName(className)[i].getElementsByClassName("emptyStarRating")[0].style.backgroundImage = "url('" + obj[i].emptyStarImage + "')" ;
-            document.getElementsByClassName(className)[i].getElementsByClassName("emptyStarRating")[0].style.backgroundRepeat="repeat-x";
-            document.getElementsByClassName(className)[i].getElementsByClassName("emptyStarRating")[0].style.width = (parseInt(obj[i].starSize)*parseInt(obj[i].maxRating) ) + "px";
-            document.getElementsByClassName(className)[i].getElementsByClassName("emptyStarRating")[0].style.height = parseInt(obj[i].starSize)+"px";
-        }
-
-        document.getElementsByClassName(className)[i].style.maxWidth = obj[i].starSize*obj[i].maxRating + "px";
-        document.getElementsByClassName(className)[i].dataset.rating = obj[i].rating;
-        document.getElementsByClassName(className)[i].dataset.step = obj[i].step;
     }
 }
 
 window.addEventListener( 'load', function() {
-    var settings =[
+    const settings =[
         {
             "rating":"3.5",
             "maxRating":"5",
